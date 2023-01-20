f6d528e4210f0b43f360af77f49208019c6dc8e7
manas parwani _TECHNONJR <115521778+Manas-tech@users.noreply.github.com>
2023-01-20 12:16:40 +0530

    Add files via upload

---
+++ agelimit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d", &t);
+	while(t--){
+	    int x,y,a;
+	    scanf("%d %d %d", &x, &y, &a);
+          if(a>=x && a<y){
+              printf("yes\n");
+          
+	    }
+	    else{
+	        printf("no\n");
+	    }
+	}
+	return 0;
+}
